ed8a862b7ab32c2f103a66effca3987dfb7a51ae
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-09-05 23:37:17 +1000

    Stored Procedures

---
+++ SQL/TransactSQL/SQLQuery2-CREATE PROCEDURE sp_SelectSchedule.sql
@@ -0,0 +1,21 @@
+USE PD_321;
+GO  -- Финализирует Statment Batch
+
+ALTER PROCEDURE sp_SelectSchedule
+AS
+BEGIN
+	SELECT
+			[ID]			=		lesson_id,
+			[Группа]		=		group_name,
+			[Дисциплина]	=		discipline_name,
+			[Преподователь]	=		FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name),
+			[День недели]	=		DATENAME(WEEKDAY,[date]),
+			[Дата]			=		[date],
+			[Время]			=		[time],
+			[Статус]		=		IIF(spent=1, N'Проведено', N'Запланированно')
+	FROM	Schedule, Teachers, Groups, Disciplines
+	WHERE	[group]		=	group_id
+	AND		discipline	=	discipline_id
+	AND		teacher		=	teacher_id
+	;
+END
